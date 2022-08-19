d5ebeda12bf067ab0a79ab7d985842df0cf02bd9
gayathri <ssgayathri2001@gmail.com>
2022-08-19 10:07:06 +0530

    Add project files.

---
+++ 19aug/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19aug
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("create a branch and push");
+            Console.ReadLine();
+        }
+    }
+}
